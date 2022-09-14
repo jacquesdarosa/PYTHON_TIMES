ea8cdfd09287d532888891959089481285953b29
Jacques da Rosa <jacquesdarosa@gmail.com>
2022-09-14 18:05:44 +0200

    finished

--- progress_bar.py
+++ progress_bar.py
@@ -12,3 +12,20 @@ Create a program that
 - saves another time moment and prints it
 - prints the time that has passed in between'''
 
+"""Peers's solution: import os       # import os
+import time     # import time
+​
+os.system("clear")  # clear terminal
+​
+print("Yeah, some funky download ...")  # print the heck out of things!
+n=4
+for i in range(0, 101, n):                      # start loop
+    line = ("-" * ((100//n) - (i//n)))          # create ----- line 100 - "#" times
+    bar = "#" * (i//n)                          # create ##### line "#" * i times
+    
+    fullbar = "|" +bar+line  + "| " + str(i) + "%"      # build progress bar
+    print(fullbar + "\r", end="")                       # print progress bar
+    time.sleep(0.1)                                     # sleep for 0,1 seconds
+​
+print("done!" + " " * 50)                               # print done" 
+"""
