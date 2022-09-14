69b26f70c4995d691a8a7b4a814d364187ec8929
Jacques da Rosa <jacquesdarosa@gmail.com>
2022-09-14 16:31:27 +0200

    finished

--- count_down.py
+++ count_down.py
@@ -11,16 +11,14 @@ def countdown(t):
  while t: # while t > 0 for clarity 
         mins = t//60
         secs = t % 60
-        mins = mins % 60
-        print(timer = '{0}:{1}:{2}'.format(int(hours), int(mins), sec))
-        '''print(timer, end="\r")'''
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
         time.sleep(1)
-        t-= 1
+        t -= 1
 t = int(input('enter the time in seconds here:'))
 start_time = time.time
 end_time =time.time
+countdown(int(t))
 
-print('Start time:', start_time)
-print('End_time:', end_time)
 print('Countdown is finished')
 
